b9c16cbdd882009361cfd56d8d9f43cd134a7a77
ISAIAS <126277721+isaias-B-Oliveira@users.noreply.github.com>
2023-08-28 08:58:20 -0300

    Update index.js

--- src/componentes/topo/index.js
+++ src/componentes/topo/index.js
@@ -5,11 +5,15 @@ export default function Topo() {
     return(
         <header>
             <div className="limitar-secao">
+        
                 <img src="assets/logo.png" alt="logomarca"/>
+        
                 <nav>
+        
                     <Link className="link-topo" to="/" >Home</Link>
                     <Link className="link-topo" to="/sabores">sabores</Link>
                     <Link className="link-topo" to="/sobre">sobre</Link>
+        
                 </nav>
             </div>
         </header>
