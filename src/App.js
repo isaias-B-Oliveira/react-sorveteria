af5a83e084bf0bfe7add27c89ee7dda5a4813a7b
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-09-19 15:41:00 -0300

    isaias

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Rotas from './rotas';
-import './App.css';
+import Rotas from "./rotas";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Rotas />
-    </div>
-  );
+    return (
+        <div>
+            <Rotas />
+        </div>
+        <></>
+    );
 }
 
 export default App;
